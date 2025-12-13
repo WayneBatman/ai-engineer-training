cdc2c403b1c8b16933b102b739cc4f28dee315e6
zhaowei <zhaowei@ebeitech.com>
2025-12-14 01:01:17 +0800

    第三周作业一的第一个部分

--- week03-homework/chunking_research/main.py
+++ week03-homework/chunking_research/main.py
@@ -1,8 +1,45 @@
+import os
+from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex
+from llama_index.llms.openai_like import OpenAILike
+from llama_index.embeddings.dashscope import DashScopeEmbedding, DashScopeTextEmbeddingModels
+from llama_index.core.node_parser import TokenTextSplitter
+
+Settings.llm = OpenAILike(
+    model="qwen-plus",
+    api_base="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    is_chat_model=True
+)
+
+Settings.embed_model = DashScopeEmbedding(
+    model_name=DashScopeTextEmbeddingModels.TEXT_EMBEDDING_V3,
+    embed_batch_size=6,
+    embed_input_length=8192
+)
+
+splitter = TokenTextSplitter(
+    chunk_size=100,
+    chunk_overlap=10,
+    separator="\n"
+)
+
+reader = SimpleDirectoryReader(input_dir="./doc")
+documents = reader.load_data()
+
 
 def main():
-# 作业的入口写在这里。你可以就写这个文件，或者扩展多个文件，但是执行入口留在这里。
-# 在根目录可以通过python -m chunking_research.main 运行
-    pass
+
+    nodes = splitter.get_nodes_from_documents(documents)
+
+    for node in nodes:
+        print(node.text)
+        print(node.metadata)
+
+    index = VectorStoreIndex.from_documents(documents)
+    query_engine = index.as_query_engine()
+    inputStr = input("请输入你想检索的内容：\n")
+    response = query_engine.query(inputStr)
+    print(response)
 
 if __name__ == "__main__":
     main()
