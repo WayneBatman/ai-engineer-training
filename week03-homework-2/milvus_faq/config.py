4970dac6750c9392a3d7600470985d1498aa2501
zhaowei <zhaowei@ebeitech.com>
2025-12-22 01:57:40 +0800

    第三周作业二的增加llamaIndex的初始化

---
+++ week03-homework-2/milvus_faq/config.py
@@ -0,0 +1,32 @@
+#一些全局设置
+import os
+
+from llama_index.core import Settings
+from llama_index.embeddings.dashscope import DashScopeEmbedding, DashScopeTextEmbeddingModels
+from llama_index.llms.openai_like import OpenAILike
+
+print("++++开始初始化++++++")
+
+#阿里千问的api_key
+DASHSCOPE_API_KEY = os.getenv("DASHSCOPE_API_KEY")
+if not DASHSCOPE_API_KEY:
+    raise ValueError("请设置环境变量 DASHSCOPE_API_KEY")
+
+Settings.llm = OpenAILike(
+    model="qwen-plus",
+    api_base="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    is_chat_model=True
+)
+
+Settings.embed_model = DashScopeEmbedding(
+    model_name=DashScopeTextEmbeddingModels.TEXT_EMBEDDING_V3,
+    embed_batch_size=6,
+    embed_input_length=8192
+)
+
+
+
+
+
+print("++++结束初始化++++++")
