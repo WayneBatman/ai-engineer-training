0bd9aeab92a966c3533bb8c7abacd3c7e8f2bd60
zhaowei <zhaowei@ebeitech.com>
2025-12-22 18:26:26 +0800

    第三周作业二的第一部分

--- week03-homework-2/milvus_faq/config.py
+++ week03-homework-2/milvus_faq/config.py
@@ -1,11 +1,36 @@
 #一些全局设置
 import os
 
+from dotenv import load_dotenv
 from llama_index.core import Settings
 from llama_index.embeddings.dashscope import DashScopeEmbedding, DashScopeTextEmbeddingModels
 from llama_index.llms.openai_like import OpenAILike
 
 print("++++开始初始化++++++")
+
+# 加载.env文件
+load_dotenv()
+
+MILVUS_HOST = os.getenv("MILVUS_HOST")
+MILVUS_PORT = os.getenv("MILVUS_PORT")
+MILVUS_URI = os.getenv("MILVUS_URI")
+MILVUS_COLLECTION_NAME = os.getenv("MILVUS_COLLECTION_NAME")
+MILVUS_DIMENSION = os.getenv("MILVUS_DIMENSION")
+MILVUS_INDEX_DIR = os.getenv("MILVUS_INDEX_DIR")
+
+print(f"MILVUS_HOST:{MILVUS_HOST}")
+print(f"MILVUS_PORT:{MILVUS_PORT}")
+print(f"MILVUS_URI:{MILVUS_URI}")
+print(f"MILVUS_COLLECTION_NAME:{MILVUS_COLLECTION_NAME}")
+print(f"MILVUS_DIMENSION:{MILVUS_DIMENSION}")
+print(f"MILVUS_DIMENSION:{MILVUS_INDEX_DIR}")
+
+# --- 数据和索引路径配置 ---
+DATA_DIR = "data"
+FAQ_FILE = os.path.join(DATA_DIR, "faq.csv")
+
+SIMILAR_TOP_K = 2
+
 
 #阿里千问的api_key
 DASHSCOPE_API_KEY = os.getenv("DASHSCOPE_API_KEY")
@@ -15,7 +40,7 @@ if not DASHSCOPE_API_KEY:
 Settings.llm = OpenAILike(
     model="qwen-plus",
     api_base="https://dashscope.aliyuncs.com/compatible-mode/v1",
-    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    api_key=DASHSCOPE_API_KEY,
     is_chat_model=True
 )
 
@@ -25,8 +50,11 @@ Settings.embed_model = DashScopeEmbedding(
     embed_input_length=8192
 )
 
-
-
+# 使用通义千问的文本嵌入模型
+EMBED_MODEL = DashScopeEmbedding(
+    model_name="text-embedding-v3",
+    api_key=DASHSCOPE_API_KEY
+)
 
 
 print("++++结束初始化++++++")
--- week03-homework-2/milvus_faq/main.py
+++ week03-homework-2/milvus_faq/main.py
@@ -1,9 +1,41 @@
+# main.py 中
+import asyncio
+import sys
+
+import uvicorn
+from fastapi import FastAPI
+from interfaceApi import router as api_router
+
 import config
+
+
+app = FastAPI(
+    title="基于Milvus的FAQ检索系统",
+    description="一个基于 LlamaIndex 和 Milvus 的 FAQ 问答 API",
+    version="0.0.1",
+    author="Wayne",
+)
+
+app.include_router(api_router,prefix="/api")
+
+@app.get("/")
+def read_root():
+    return {"message": "欢迎使用 Milvus FAQ 检索系统 API. 请访问 /docs 查看详情."}
+
+
 
 def main():
 # 作业的入口写在这里。你可以就写这个文件，或者扩展多个文件，但是执行入口留在这里。
 # 在根目录可以通过python -m milvus_faq.main 运行
-    pass
+    # 直接使用变量
+    print("启动 FastAPI 服务...")
+    # 打印一些启动信息
+    print(f"数据文件路径: {config.FAQ_FILE}")
+    print(f"Milvus Lite 数据库路径: {config.MILVUS_URI}")
+    print(f"嵌入模型: {config.EMBED_MODEL.model_name}")
+
+    # 运行 FastAPI 应用
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
     main()
